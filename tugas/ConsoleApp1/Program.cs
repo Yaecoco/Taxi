62399a61ccc8b9708c331c12183b43287ec211bf
user <user@DESKTOP-LEPJD0T>
2023-03-21 21:35:27 +0700

    Add project files.

---
+++ tugas/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek Taxi
+            Taxi taxi= new Taxi();
+
+            // pengertian nilai properties
+            taxi.DriverName = "Arya";
+            taxi.OnDuty = true;
+            taxi.NumPassanger = 9;
+
+            // pemanggil method
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOffPassanger();
+
+            Console.ReadKey();
+        }
+    }
+}
+    
